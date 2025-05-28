d5f385154d377b7c256dd83eb436b8f8087e6a10
Editalthte <jordan.klinefelter+github@protonmail.com>
2025-05-28 16:37:57 -0400

    - Initial draft/stub of config handler

---
+++ src/config_manager.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <fstream>
+#include <map>
+#include <nlohmann/json.hpp> // https://github.com/nlohmann/json
+#include <string>
+
+using nlohmann::json;
+using std::string;
+
+enum Control_Type
+{
+	keyboard,
+	mouse,
+	joystick
+};
+
+class Config_Manager
+{
+	
+};
+
+struct Controls_Config
+{
+	string up_key;
+	string down_key;
+	string left_key;
+	string right_key;
+
+	string fire_key;
+	string rear_mode_change_key;
+	string sidekick_left_key;
+	string sidekick_right_key;
+};
+
+struct Video_Config
+{
+	bool fullscreen;
+	int scaler;
+	string scaling_mode;
+};
+
+struct Config
+{
+	std::map<Control_Type, Controls_Config> controls_config;
+	Video_Config video_config;
+};
+
+
+
