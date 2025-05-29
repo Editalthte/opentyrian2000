fad3e37be7243a359a145a369b3bbbb6263fe508
Editalthte <jordan.klinefelter+github@protonmail.com>
2025-05-29 09:56:41 -0400

    - Most keyboard/mouse handler logic implement at basic level

--- src/input_handler.hpp
+++ src/input_handler.hpp
@@ -5,14 +5,28 @@
 #include "SDL2/SDL_version.h"
 #include "SDL2/SDL_hints.h"
 #include "SDL2/SDL_events.h"
-#include <SDL2/SDL_stdinc.h>
+#include "SDL2/SDL_stdinc.h"
+#include "SDL2/SDL_scancode.h"
+#include "SDL2/SDL_keycode.h"
+#include "SDL2/SDL_timer.h"
+
+#include <map>
+#include <string>
+
+using std::string;
+
+const int SDL_POLL_INTERVAL = 10;
 
 class Keyboard
 {
 	public:
 
+		// Data Members
 		bool new_key{false};
-		bool keydown{false};
+		bool key_down{false};
+		Uint8 active_keys[SDL_NUM_SCANCODES];
+		SDL_Scancode last_key_scancode;
+		Uint16 last_key_modifier;
 
 		// Default Constructor
 		Keyboard()
@@ -23,54 +37,96 @@ class Keyboard
 				SDL_SetHint(SDL_HINT_MOUSE_RELATIVE_SYSTEM_SCALE, "1");
 			#endif
 		}
-
 };
 
 class Joystick
 {
+	public:
 
+		// Data Members
+		bool joy_down;
 };
 
 class Mouse
 {
-	private:
-
-
 	public:
 
+		// Data Members
 		bool mouse_down{false};
 		bool new_mouse{false};
+		bool relative_mode_setting; // Represents if the user *wants* this to occur at all
 		Uint8 most_recent_mouse_button;
 		
 		Sint32 mouse_position_x;
 		Sint32 mouse_position_y;
 
+		Sint32 last_mouse_position_x;
+		Sint32 last_mouse_position_y;
+
 		Sint32 relative_mouse_position_x;
 		Sint32 relative_mouse_position_y;
-		bool relative_mode_enabled;
 
+		bool inactive;
+
+		
+
+
+		struct Button
+		{
+			// Data Members
+			int id;
+			bool pressed;
+
+			// Default Constructor
+			Button(const int INIT_ID, const bool INIT_PRESSED = false)
+			{
+				this->id = INIT_ID;
+				this->pressed = INIT_PRESSED;
+			}
+
+			// case 1: // Fire Main Weapons
+			// mouse_pressed[0] = mousedown;
+			// break;
+			// case 2: // Fire Left Sidekick
+			// mouse_pressed[1] = mousedown;
+			// break;
+			// case 3: // Fire Right Sidekick
+			// mouse_pressed[2] = mousedown;
+			// break;
+			// case 4: // Fire BOTH Sidekicks
+			// mouse_pressed[1] = mousedown;
+			// mouse_pressed[2] = mousedown;
+			// break;
+			// case 5: // Change Rear Mode
+			// mouse_pressed[3] = mousedown;
+			// break;
+		};
+
+		std::map<int, Button> buttons
+		{
+			{0, Button(0)},
+			{1, Button(1)},
+			{2, Button(2)},
+			{3, Button(3)}
+		};
 
 		// Default Constructor
 		Mouse()
 		{ }
 
-		void relative_mode_enable()
-		{ this->set_relative_mode(true); }
-
-		void relative_mode_disable()
-		{ this->set_relative_mode(false); }
-
-		void set_relative_mode(bool enable)
+		// This should probably be moved elsewhere, as this isn't really a concern of the mouse itself
+		void set_sdl_relative_mode(bool WINDOW_HAS_FOCUS)
 		{
 			// Stupid hacky "casting"
-			SDL_bool sdl_enable = enable ? SDL_bool::SDL_TRUE : SDL_bool::SDL_FALSE;
+			SDL_bool sdl_enable = (this->relative_mode_setting && WINDOW_HAS_FOCUS) ? SDL_bool::SDL_TRUE : SDL_bool::SDL_FALSE;
 			SDL_SetRelativeMouseMode(sdl_enable);
-
-			this->relative_mode_enabled = enable;
 
 			this->relative_mouse_position_x = 0;
 			this->relative_mouse_position_y = 0;
 		}
+
+
+		// Methods
 
 		unsigned short mousePosition(unsigned short *mouse_x_position, unsigned short *mouse_y_position)
 		{
@@ -85,7 +141,7 @@ class Mouse
 		{
 			// service_SDL_events(false);
 
-			// scaleWindowDistanceToScreen(&mouseWindowXRelative, &mouseWindowYRelative);
+			// scaleWindowDistanceToScreen(&mouseWindowXRelative, &mouseWindowYRelative);  // video.h
 			*out_x = this->relative_mouse_position_x;
 			*out_y = this->relative_mouse_position_y;
 
@@ -105,9 +161,13 @@ class Input_Handler
 		Joystick joystick;
 
 		bool new_text;
+		char last_text[SDL_TEXTINPUTEVENT_TEXT_SIZE];
+
 		bool window_has_focus;
 
 		// Methods
+		void set_sdl_relative_mode()
+		{ this->mouse.set_sdl_relative_mode(this->window_has_focus); }
 
 		void service_SDL_events(bool clear_new)
 		{
@@ -126,130 +186,183 @@ class Input_Handler
 				switch (sdl_event.type)
 				{
 					case SDL_WINDOWEVENT:
+					{
 						switch (sdl_event.window.event)
 						{
 							case SDL_WINDOWEVENT_FOCUS_LOST:
+							{
 								this->window_has_focus = false;
-
-								this->mouse.set_relative_mode(this->mouse.relative_mode_enabled);
-								break;
-
-							case SDL_WINDOWEVENT_FOCUS_GAINED:
-								this->window_has_focus = true;
-
-								mouseSetRelative(mouseRelativeEnabled);
-								break;
-
-							case SDL_WINDOWEVENT_RESIZED:
-								video_on_win_resize();
+								this->set_sdl_relative_mode();
 								break;
 							}
+
+							case SDL_WINDOWEVENT_FOCUS_GAINED:
+							{
+								this->window_has_focus = true;
+								this->set_sdl_relative_mode();
+								break;
+							}
+
+							case SDL_WINDOWEVENT_RESIZED:
+							{
+								// video_on_win_resize(); // video.h
+								break;
+							}
+						}
+
 						break;
+					}
 
 					case SDL_KEYDOWN:
+					{
 						/* <alt><enter> toggle fullscreen */
 						if (sdl_event.key.keysym.mod & KMOD_ALT && sdl_event.key.keysym.scancode == SDL_SCANCODE_RETURN)
 						{
-							toggle_fullscreen();
+							// toggle_fullscreen(); // video.h
 							break;
 						}
 
-						keysactive[ev.key.keysym.scancode] = 1;
+						this->keyboard.active_keys[sdl_event.key.keysym.scancode] = 1;
+						this->keyboard.new_key = true;
 
-						newkey = true;
-						lastkey_scan = ev.key.keysym.scancode;
-						lastkey_mod = ev.key.keysym.mod;
-						keydown = true;
+						this->keyboard.last_key_scancode = sdl_event.key.keysym.scancode;
+						this->keyboard.last_key_modifier = sdl_event.key.keysym.mod;
+						this->keyboard.key_down = true;
 
-						mouseInactive = true;
+						this->mouse.inactive = true;
+
 						return;
+					}
+
 
 					case SDL_KEYUP:
-						keysactive[ev.key.keysym.scancode] = 0;
-						keydown = false;
+					{
+						this->keyboard.active_keys[sdl_event.key.keysym.scancode] = 0;
+						this->keyboard.key_down = false;
+
 						return;
+					}
+
 
 					case SDL_MOUSEMOTION:
-						mouse_x = ev.motion.x;
-						mouse_y = ev.motion.y;
-						mapWindowPointToScreen(&mouse_x, &mouse_y);
+					{
+						this->mouse.mouse_position_x = sdl_event.motion.x;
+						this->mouse.mouse_position_y = sdl_event.motion.y;
+						// mapWindowPointToScreen(&this->mouse.mouse_position_x, &this->mouse.mouse_position_y); // video.h
 
-						if (mouseRelativeEnabled && windowHasFocus)
+						if (this->mouse.relative_mode_setting && this->window_has_focus)
 						{
-							mouseWindowXRelative += ev.motion.xrel;
-							mouseWindowYRelative += ev.motion.yrel;
+							this->mouse.relative_mouse_position_x += sdl_event.motion.xrel;
+							this->mouse.relative_mouse_position_y += sdl_event.motion.yrel;
 						}
 
 						// Show system mouse pointer if outside screen.
-						SDL_ShowCursor(mouse_x < 0 || mouse_x >= vga_width ||
-									mouse_y < 0 || mouse_y >= vga_height ? SDL_TRUE : SDL_FALSE);
+						const int VGA_WIDTH = 320;
+						const int VGA_HEIGHT = 200;
+						SDL_ShowCursor(
+					this->mouse.mouse_position_x < 0 || 
+							this->mouse.mouse_position_x >= VGA_WIDTH ||
+							this->mouse.relative_mouse_position_y < 0 ||
+							this->mouse.relative_mouse_position_y >= VGA_HEIGHT ? SDL_TRUE : SDL_FALSE
+						);
 
-						if (ev.motion.xrel != 0 || ev.motion.yrel != 0)
-							mouseInactive = false;
+						if (sdl_event.motion.xrel != 0 || sdl_event.motion.yrel != 0)
+						{ this->mouse.inactive = false; }
+						
 						break;
+					}
 
 					case SDL_MOUSEBUTTONDOWN:
-						mouseInactive = false;
-
-						// fall through
+					{ this->mouse.inactive = false; }
+						
+						// fall through -- does the brace syntax prevent fallthrough?
 					case SDL_MOUSEBUTTONUP:
-						mapWindowPointToScreen(&sdl_event.button.x, &sdl_event.button.y);
+					{
+						// mapWindowPointToScreen(&sdl_event.button.x, &sdl_event.button.y); // video.h
+
 						if (sdl_event.type == SDL_MOUSEBUTTONDOWN)
 						{
-							newmouse = true;
-							most_recent_mouse_button = ev.button.button;
-							lastmouse_x = ev.button.x;
-							lastmouse_y = ev.button.y;
-							mousedown = true;
-						}
-						else
-						{
-							mousedown = false;
+							this->mouse.new_mouse = true;
+							this->mouse.most_recent_mouse_button = sdl_event.button.button;
+	
+							this->mouse.last_mouse_position_x = sdl_event.button.x;
+							this->mouse.last_mouse_position_y = sdl_event.button.y;
+							this->mouse.mouse_down = true;
 						}
 
-						int whichMB = -1;
+						else
+						{ this->mouse.mouse_down = false; }
+
+
+						int mouse_button_id = -1;
 						switch (sdl_event.button.button)
 						{
-							case SDL_BUTTON_LEFT:   whichMB = 0; break;
-							case SDL_BUTTON_RIGHT:  whichMB = 1; break;
-							case SDL_BUTTON_MIDDLE: whichMB = 2; break;
+							case SDL_BUTTON_LEFT:   mouse_button_id = 0; break;
+							case SDL_BUTTON_RIGHT:  mouse_button_id = 1; break;
+							case SDL_BUTTON_MIDDLE: mouse_button_id = 2; break;
 						}
-						if (whichMB < 0)
-							break;
+	
+						if (mouse_button_id < 0)
+						{ break; }
+						
+						// TODO: replace with actual settings/configuration logic
+						const uint8_t MOUSE_SETTINGS[3]{ 1, 4, 5 };
 
-						switch (mouseSettings[whichMB])
+						switch (MOUSE_SETTINGS[mouse_button_id])
 						{
 							case 1: // Fire Main Weapons
-								mouse_pressed[0] = mousedown;
+							{
+								this->mouse.buttons[0].pressed = this->mouse.mouse_down;
 								break;
+							}
+
 							case 2: // Fire Left Sidekick
-								mouse_pressed[1] = mousedown;
-								break;
+							{
+								this->mouse.buttons[1].pressed = this->mouse.mouse_down;
+								break;								
+							}
+
 							case 3: // Fire Right Sidekick
-								mouse_pressed[2] = mousedown;
-								break;
+							{
+								this->mouse.buttons[2].pressed = this->mouse.mouse_down;
+								break;								
+							}
+
 							case 4: // Fire BOTH Sidekicks
-								mouse_pressed[1] = mousedown;
-								mouse_pressed[2] = mousedown;
-								break;
+							{
+								this->mouse.buttons[1].pressed = this->mouse.mouse_down;
+								this->mouse.buttons[2].pressed = this->mouse.mouse_down;
+								break;								
+							}
+
 							case 5: // Change Rear Mode
-								mouse_pressed[3] = mousedown;
-								break;
+							{
+								this->mouse.buttons[3].pressed = this->mouse.mouse_down;
+								break;								
+							}
 						}
+
 						break;
+					}
+					
 
 					case SDL_TEXTINPUT:
-						SDL_strlcpy(last_text, ev.text.text, COUNTOF(last_text));
-						new_text = true;
-						break;
+					{
+						SDL_strlcpy(this->last_text, sdl_event.text.text, COUNTOF(this->last_text));
+						this->new_text = true;
+
+						break;						
+					}
 
 					case SDL_TEXTEDITING:
-						break;
+					{ break; }
 
 					case SDL_QUIT:
+					{
 						/* TODO: Call the cleanup code here. */
 						exit(0);
-						break;
+						break;						
+					}
 				}
 			}
 		}
@@ -260,26 +373,32 @@ class Input_Handler
 		Input_Handler()
 		{ }
 
+		// Methods
 		void flush_events_buffer(void)
 		{
 			SDL_Event sdl_event;
 			while (SDL_PollEvent(&sdl_event));
 		}
 
-		// void wait_input(bool keyboard, bool mouse, bool joystick)
-		// {
-		// 	service_SDL_events(false);
-		// 	while (!((keyboard && keydown) || (mouse && mousedown) || (joystick && joydown)))
-		// 	{
-		// 		SDL_Delay(SDL_POLL_INTERVAL);
-		// 		push_joysticks_as_keyboard();
-		// 		service_SDL_events(false);
+		void wait_input(bool keyboard_bool, bool mouse_bool, bool joystick_bool)
+		{
+			service_SDL_events(false);
 
-		// #ifdef WITH_NETWORK
-		// 		if (isNetworkGame)
-		// 		{ network_check(); }
-					
-		// #endif
-		// 	}
-		// }
+			// Continue to poll for input on all devices until input is detected
+			while (
+				!(
+					(keyboard_bool && this->keyboard.key_down) ||
+					(mouse_bool && this->mouse.mouse_down) ||
+					(joystick_bool && this->joystick.joy_down)
+				)
+			)
+			{
+				SDL_Delay(SDL_POLL_INTERVAL);
+				// push_joysticks_as_keyboard();
+				service_SDL_events(false);
+
+				// if (isNetworkGame)
+				// { network_check(); }
+			}
+		}
 };
